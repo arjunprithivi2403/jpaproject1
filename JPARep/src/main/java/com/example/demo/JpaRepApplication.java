650f8ac61c01ef0aa8df6727cadb8e8eb7cd8b12
ARJUN PRITHIVI M <108717764+arjunprithivi2403@users.noreply.github.com>
2023-08-21 16:37:49 +0530

    Add files via upload

---
+++ JPARep/src/main/java/com/example/demo/JpaRepApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaRepApplication.class, args);
+	}
+
+}
